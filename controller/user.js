dd187c383720d1e845fe16de543929aa25a01b8f
George Charles <gkcharles@outlook.com>
2021-07-21 18:14:12 +0100

    add user.js and weather.js

---
+++ controller/user.js
@@ -0,0 +1,9 @@
+exports.createUser = (req, res) => {};
+
+exports.getUsers = (req, res) => {};
+
+exports.getUser = (req, res) => {};
+
+exports.updateUser = (req, res) => {};
+
+exports.deleteUser = (req, res) => {};
