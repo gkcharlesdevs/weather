50de16e78101e5fb8083929d40dac8491627094e
George Charles <gkcharles@outlook.com>
2021-07-21 18:19:34 +0100

    update files

--- controller/user.js
+++
@@ -1,9 +0,0 @@
-exports.createUser = (req, res) => {};
-
-exports.getUsers = (req, res) => {};
-
-exports.getUser = (req, res) => {};
-
-exports.updateUser = (req, res) => {};
-
-exports.deleteUser = (req, res) => {};
--- controller/weather.js
+++
@@ -1,9 +0,0 @@
-exports.createWeather = (req, res) => {};
-
-exports.getWeathers = (req, res) => {};
-
-exports.getWeather = (req, res) => {};
-
-exports.updateWeather = (req, res) => {};
-
-exports.deleteWeather = (req, res) => {};
