3373ae8b690e43cbdd74e6290d255285a89159cf
George Charles <gkcharles@outlook.com>
2021-07-21 18:18:08 +0100

    add rename model, rename controller, add routes

---
+++ controllers/weather.js
@@ -0,0 +1,9 @@
+exports.createWeather = (req, res) => {};
+
+exports.getWeathers = (req, res) => {};
+
+exports.getWeather = (req, res) => {};
+
+exports.updateWeather = (req, res) => {};
+
+exports.deleteWeather = (req, res) => {};
